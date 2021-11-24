21481c658cac7ff9006fd8c05458c98fa5408d5d
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 23:52:28 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -33,7 +33,7 @@ export default function SurfSpotSection({
             <div id="videoPhrase"></div>
             <p class="mb-2">You can <a href={forecastLink} target="_blank">find {titleContraction} {forecastPhrase} surf report and surf forecast here.</a></p>
             <p class="mb-2">Get Google Maps <a href={directionsLink} target="blank">directions to {titleContraction} surf parking lot.</a></p>
-            <p class="mb-2">Get information about the surf spots nearby {title} in the sidebar</p>
+            <p class="mb-2">We have more information in our database. Please email usGet information about the surf spots nearby {title} in the sidebar</p>
         </div >
     )
 }
