c7191431c2554834e23f0f771c2fb236acd05523
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 18:54:26 +0000

    PostLayout.js: changed

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -99,7 +99,7 @@ export default function PostLayout(props) {
         bestPeriod = '16+ seconds';
     }
 
-    // Set period phrase
+    // Set swell direction
 	if (swellDir === 'W') {
 		swellDir = 'West';
 	} else if (swellDir === 'SW') {
