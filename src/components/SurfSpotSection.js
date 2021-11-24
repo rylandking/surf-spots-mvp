e22ecdfc6be6799200820e94145751ac52619b79
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 22:53:51 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -16,8 +16,8 @@ export default function SurfSpotSection({
     skill }) {
     return (
         <div>
-            <p>{titleContraction} surf is {skillPhrase} {waveDirection} {waveType} break.</p>
-            <p>The waves at {title} are at their best with a {bestPeriod} {bestSwellDirection} swell at around {bestSize}. If you get it with a light {bestWind} wind at {bestTide} tide, it’ll be close to as good as it gets — it’s a {stars} star surf spot, so that means the surf would be {starsDesc} for {skill} surfers.</p>
+            <p className="mb-2">{titleContraction} surf is {skillPhrase} {waveDirection} {waveType} break.</p>
+            <p className="mb-2">The waves at {title} are at their best with a {bestPeriod} {bestSwellDirection} swell at around {bestSize}. If you get it with a light {bestWind} wind at {bestTide} tide, it’ll be close to as good as it gets — it’s a {stars} star surf spot, so that means the surf would be {starsDesc} for {skill} surfers.</p>
         </div >
     )
 }
