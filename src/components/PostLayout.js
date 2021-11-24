4c737c041b5c243188e8507326089e6613bbcec0
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 23:24:16 +0000

    PostLayout.js: changed

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -310,9 +310,9 @@ export default function PostLayout(props) {
         videoSearchURL = `https://www.google.com/search?q=${title}%20surf&tbm=vid`;
 
         videoPhrase = `
-            <p class="mb-4">Watch this video of ${titleContraction} surf to get a better feel for what the waves are like.</p>
+            <p class="mb-3">Watch this video of ${titleContraction} surf to get a better feel for what the waves are like.</p>
             ${videoEmbed}
-            <p class="mb-2"> Remember, this is just one day. Always check the surf report before going. You can <a href="${videoSearchURL}" target="_blank">watch other videos of ${titleContraction} surf here</a>.</p>
+            <p class="mb-3"> Remember, this is just one day. Always check the surf report before going. You can <a href="${videoSearchURL}" target="_blank">watch other videos of ${titleContraction} surf here</a>.</p>
         `;
     } else {
         videoSearchURL = `https://www.google.com/search?q=${title}&tbm=vid`;
