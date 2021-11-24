e257a7ef8f811108c70c2b3b0b4df5a6daa92549
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 19:07:45 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 export default function SurfSpotSection({ titleContraction }) {
     return (
         <div>
-            <p>{titleContraction} surf is ${skillPhrase} ${waveDirection} reef break.</p>
+            <p>{titleContraction} surf is ${skillPhrase} ${waveDirection} ${wavetype} break.</p>
         </div >
     )
 }
