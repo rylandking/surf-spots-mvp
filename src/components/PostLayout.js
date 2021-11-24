bca78b912ac6ea37a37889e41a2001411aece01e
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 23:37:43 +0000

    PostLayout.js: changed

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -17,7 +17,7 @@ export default function PostLayout(props) {
         .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
         .join(' ');
     let titleContraction;
-    let surfSpotLatLng = page.surfSpotLatLng.lat;
+    let surfSpotLat = page.surfSpotLatLng.lat;
     let surfSpotLatLng = page.surfSpotLatLng.lng;
     console.log(page);
     
