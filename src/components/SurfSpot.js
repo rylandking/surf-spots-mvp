2a42a387bb210c24b7b4e0ab976bd09f500a89ee
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 18:02:18 +0000

    SurfSpot.js: removed

--- src/components/SurfSpot.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export default function SurfSpotSection({ }) {
-    return (
-        <div>
-            <p>Pipeline surf is an intermediate right reef break.</p>
-        </div >
-    )
-}
