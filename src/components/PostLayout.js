e390c7bffdf79e8ef1276b41e7a01a8fea17845b
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 19:05:44 +0000

    PostLayout.js: changed

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -317,7 +317,7 @@ export default function PostLayout(props) {
         videoSearchURL = `https://www.google.com/search?q=${title}&tbm=vid`;
 
         videoPhrase = `
-      <p class="mb-2">You can <a href="${videoSearchURL}" target="_blank">watch videos of ${title} surf here</a>.</p>
+      <p class="mb-2">You can <a href="${videoSearchURL}" target="_blank">watch videos of ${titleContraction} surf here</a>.</p>
     `;
     }
 
