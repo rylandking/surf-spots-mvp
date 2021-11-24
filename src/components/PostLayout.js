c8709e570bc1e65edfa59a8b0589839bf9098712
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 19:12:11 +0000

    PostLayout.js: changed

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -28,7 +28,7 @@ export default function PostLayout(props) {
     let bestTide = page.bestTide;
     let bestWind = page.bestWind;
     let bestSwellDirection = page.bestSwellDirection;
-    let waveType
+    let waveType = page.wave
     let bottomType
     let isBigWave;
     let isBarrel;
