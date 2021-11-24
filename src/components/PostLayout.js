5252ab0fd764c9e484fd711f3698d7a824613b08
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 23:17:21 +0000

    PostLayout.js: changed

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -349,7 +349,6 @@ export default function PostLayout(props) {
                             bestMonthsPhrase={bestMonthsPhrase}
                             videoPhrase={videoPhrase}
                         />
-
                         {page.markdown_content && (
                             <Markdown options={{ forceBlock: true }} className="sb-markdown max-w-screen-md mx-auto" data-sb-field-path="markdown_content">
                                 {page.markdown_content}
