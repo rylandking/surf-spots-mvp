b8290950be1775a84d7664cad7060d64fb08556a
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 23:19:13 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -18,7 +18,7 @@ export default function SurfSpotSection({
     videoPhrase }) {
 
     window.onload = function what(){
-        document.getElementById('hello').innerHTML = 'hi';
+        document.getElementById('videoPhrase').innerHTML = 'hi';
     };
     document.getElementById("videoPhrase").innerHTML = videoPhrase;
     return (
