5397903d5f5a62093510b61afa9bd1f82ebf9d5a
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 19:00:23 +0000

    PostLayout.js: changed

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -37,6 +37,11 @@ export default function PostLayout(props) {
     let starsDesc;
     let forecastLink;
     let videoLink;
+    	let video = data.video;
+	let youTubeID;
+	let videoEmbed;
+	let videoSearchURL;
+	let videoPhrase;
     let bestBoard;
     let janRating;
     let febRating;
