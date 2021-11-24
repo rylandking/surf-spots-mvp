4990b694ec2fdb08da0ecdccaec0688e160c06f5
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 22:48:50 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -13,7 +13,7 @@ bestWind }) {
     return (
         <div>
             <p>{titleContraction} surf is {skillPhrase} {waveDirection} {waveType} break.</p>
-            <p>The waves at {title} are at their best with a ${bestPeriod} ${bestSwellDirection} swell at around ${bestSize}. If you get it with a light ${windDir} wind at ${tide} tide, it’ll be close to as good as it gets — it’s a ${stars} star surf spot, so that means the surf would be {starDesc} for {skill} surfers.</p>
+            <p>The waves at {title} are at their best with a ${bestPeriod} ${bestSwellDirection} swell at around ${bestSize}. If you get it with a light ${bestWind} wind at ${tide} tide, it’ll be close to as good as it gets — it’s a ${stars} star surf spot, so that means the surf would be {starDesc} for {skill} surfers.</p>
         </div >
     )
 }
