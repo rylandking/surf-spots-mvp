0dc2f2aae501fdd72f0057fb4bb9eb993b2ef16b
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 19:17:41 +0000

    PostLayout.js: changed

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -32,7 +32,7 @@ export default function PostLayout(props) {
     let bottomType
     let isBigWave;
     let isBarrel;
-    let waveDirection = page.waveDirection.value;
+    let waveDirection = page.waveDirection;
     let waveQuality;
     let stars;
     let starsDesc;
