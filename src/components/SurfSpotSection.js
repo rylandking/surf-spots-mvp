03a1ad27f51946f84a8a373649709cbf5174e4ac
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 18:02:18 +0000

    SurfSpotSection.js: added

---
+++ src/components/SurfSpotSection.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function SurfSpotSection({ }) {
+    return (
+        <div>
+            <p>Pipeline surf is an intermediate right reef break.</p>
+        </div >
+    )
+}
