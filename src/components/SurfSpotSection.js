82998e7a296c9120ff432d78f1104f07833b1d32
rylandking <ryland@stackbit.com>
2021-11-24 17:14:21 -0700

    copy to surf spot

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -19,22 +19,42 @@ export default function SurfSpotSection({
     forecastLink,
     forecastPhrase,
     directionsLink,
-    localsDescription }) {
-
+    localsDescription
+}) {
     useEffect(() => {
         window.onload = function setVideoPhrase() {
             document.getElementById('videoPhrase').innerHTML = videoPhrase;
         };
-    })
+    });
     return (
         <div>
-            <p className="mb-2">{titleContraction} surf is {skillPhrase} {waveDirection} {waveType} break.</p>
-            <p className="mb-2">The waves at {title} are at their best with a {bestPeriod} {bestSwellDirection} swell at around {bestSize}. If you get it with a light {bestWind} wind at {bestTide} tide, it’ll be close to as good as it gets. {title} is a {stars} star surf spot, so that means the surf would be {starsDesc} for {skill} and above surfers.</p>
-            <p class="mb-2">The best time of year to surf {title} {bestMonthsPhrase}.</p>
+            <p className="mb-2">
+                {titleContraction} surf is {skillPhrase} {waveDirection} {waveType} break.
+            </p>
+            <p className="mb-2">
+                The waves at {title} are at their best with a {bestPeriod} {bestSwellDirection} swell at around {bestSize}. If you get it with a light{' '}
+                {bestWind} wind at {bestTide} tide, it’ll be close to as good as it gets. {title} is a {stars} star surf spot, so that means the surf would be{' '}
+                {starsDesc} for {skill} and above surfers.
+            </p>
+            <p className="mb-2">
+                The best time of year to surf {title} {bestMonthsPhrase}.
+            </p>
             <div id="videoPhrase"></div>
-            <p class="mb-2">You can <a href={forecastLink} target="_blank">find {titleContraction} {forecastPhrase} surf report and surf forecast here.</a></p>
-            <p class="mb-2"><a href={directionsLink} target="blank">Click here for Google Maps directions to {titleContraction} surf parking lot.</a></p>
-            <p class="mb-2">I have more info in my database. Please <a href="mailto:heyryland@gmail.com">email me</a> what else would be helpful.</p>
-        </div >
-    )
+            <p className="mb-2">
+                You can{' '}
+                <a href={forecastLink} target="_blank" rel="noreferrer">
+                    find {titleContraction} {forecastPhrase} surf report and surf forecast here.
+                </a>
+            </p>
+            <p className="mb-2">
+                <a href={directionsLink} target="blank">
+                    Click here for Google Maps directions to {titleContraction} surf parking lot.
+                </a>
+            </p>
+            <h3 className="mt-6 mb-2">{titleContraction} Local Description</h3>
+            <p className="mb-2">{localsDescription}</p>
+            <h3 className="mt-6 mb-2">Want something specific?</h3>
+            <p className="mb-2">I have more info in my database. Please email me (heyryland @ gmail) what else would be helpful.</p>
+        </div>
+    );
 }
