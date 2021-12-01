d01ee761a4f26f56da9577c2af117be4249b646d
Stackbit Code Editor <projects@stackbit.com>
2021-12-01 03:48:11 +0000

    PostLayout.js: changed

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -19,8 +19,6 @@ export default function PostLayout(props) {
     let titleContraction;
     let surfSpotLat = page.surfSpotCoords[0].lat;
     let surfSpotLng = page.surfSpotCoords[0].lng;
-    console.log(surfSpotLat, surfSpotLng);
-
     let directionsLink = `https://www.google.com/maps?saddr=My+Location&daddr=${surfSpotLat},${surfSpotLng}`;
     let mapZoom = page.mapZoom;
     let parkingLatLng = page.parkingLatLng;
