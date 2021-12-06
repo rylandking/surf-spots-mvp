dc4a86203a5c3a6efc86aa676184cb03260b361b
Stackbit Code Editor <projects@stackbit.com>
2021-12-06 23:39:01 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -52,11 +52,11 @@ export default function SurfSpotSection({
                 </a>
             </p>
             <div>
-            {(localsDescription.length > 20) ?
-                <h3 className="mt-6 mb-2">Local Description of Surfing {title}</h3>
-                <p className="mb-2">{localsDescription}</p>
+                {(localsDescription.length > 20) ?
+                    <h3 className="mt-6 mb-2">Local Description of Surfing {title}</h3>
+                    <p className="mb-2">{localsDescription}</p>
             : }
-            <h3 className="mt-6 mb-2">Want something specific?</h3>
+                <h3 className="mt-6 mb-2">Want something specific?</h3>
             </div>
             <p className="mb-2">I have more info in my database. Please email me (heyryland @ gmail) what else would be helpful.</p>
         </div>
