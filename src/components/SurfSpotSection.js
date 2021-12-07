f7f3f756dc6f2b165647b31914aebded3efbc654
Stackbit Code Editor <projects@stackbit.com>
2021-12-07 00:11:20 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -27,24 +27,6 @@ export default function SurfSpotSection({
         };
     });
 
-    let youTubeID;
-    let videoEmbed;
-    let videoSearchURL;
-
-    // Set video phrase
-    if (videoLink) {
-        if (videoLink.includes('youtu.be')) {
-            youTubeID = videoLink.split('.be/')[1];
-            youTubeID = youTubeID.split('?')[0];
-        } else {
-            youTubeID = videoLink.split('v=')[1];
-        }
-        videoEmbed = `<iframe class="mb-3" width="560" height="315" src="https://www.youtube-nocookie.com/embed/${youTubeID}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
-        videoSearchURL = `https://www.google.com/search?q=${title}%20surf&tbm=vid`;
-    } else {
-        videoSearchURL = `https://www.google.com/search?q=${title}%20surf&tbm=vid`;
-    }
-
     return (
         <div>
             <p className="mb-2">
