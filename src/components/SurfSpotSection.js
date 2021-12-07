876573489cd7fad0fa1787a50f7c5fe567c88c79
Stackbit Code Editor <projects@stackbit.com>
2021-12-07 00:09:40 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -68,7 +68,7 @@ export default function SurfSpotSection({
             </p>
             <p class="mb-3">Watch this video of {titleContraction} surf to get a better feel for what the waves are like.</p>
             <div>{videoEmbed}</div>
-            <div id="videoPhrase"></div>
+            <div id="videoEmbed"></div>
             <p class="mb-3"> Remember, this is just one day. Always check the surf report before going.</p>
             <p class="mb-2">You can <a href={videoSearchURL} target="_blank">watch videos of {titleContraction} surf here</a>.</p>
             <p className="mb-2">
