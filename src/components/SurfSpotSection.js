9baaf4cd413a76887d1df86cdc0efdbb19f9b948
Stackbit Code Editor <projects@stackbit.com>
2021-12-07 00:09:43 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -24,7 +24,7 @@ export default function SurfSpotSection({
 }) {
     useEffect(() => {
         window.onload = function setVideoPhrase() {
-            document.getElementById('videoPhrase').innerHTML = videoEmbed;
+            document.getElementById('videoEmbed').innerHTML = videoEmbed;
         };
     });
 
