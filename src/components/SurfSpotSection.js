5560e3ae6ddb1f5026fef05c8453292397d00165
rylandking <ryland@stackbit.com>
2021-12-08 06:30:20 -0700

    no idea again

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -53,7 +53,7 @@ export default function SurfSpotSection({
             </p>
             <div>
                 <h3 className="mt-6 mb-2">Local Description of Surfing {title}</h3>
-                <p className="mb-2">{(localsDescription.length > 20) ? { localsDescription } : `Email me to add a detailed description.`}</p>
+                <p className="mb-2">{localsDescription.length > 20 ? { localsDescription } : `Email me to add a detailed description.`}</p>
                 <h3 className="mt-6 mb-2">Want something specific?</h3>
             </div>
             <p className="mb-2">I have more info in my database. Please email me (heyryland @ gmail) what else would be helpful.</p>
